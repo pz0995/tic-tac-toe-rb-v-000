82d8321e345703d9f8da90fe9d65cf0f827775a2
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 04:27:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,23 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  number_of_turns = 0
+  board.each do |el|
+    if el == "X" || el == "O"
+      number_of_turns += 1
+    end
+  end
+  number_of_turns
+end
 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else turn_count(board) % 2 == 1
+      return "O"
+    end
+  end
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
